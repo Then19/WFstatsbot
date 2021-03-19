1c7a130d4446d916e0e628a62afe7d96b66c4f93
Then19 <tol9hstar@gmail.com>
2021-03-19 13:37:12 +0500

    lotteryUPD

--- lottery.py
+++ lottery.py
@@ -2,11 +2,11 @@ pincodes = ['H&K 121(1 д.)  BR7N2V73ZPMHF2MT7425',
             'FN FAL DSA-58 «Эфир» (1 д.)  BU1PJ30TQGMYPIEWNBEK',
             'Winchester 1887 «Эфир» (1 д.)  M6FTXIZ96C60LQE3ZB25',
             'LWRC SMG-45 «Эфир» (1 д.)  HO9BLS6H7RG32UGNHJYX',
-            'Desert Tech MDR‐C «Эфир» (1 д.))  SHUF8F11A3I3O35K3760',
-            'Desert Tech MDR‐C «Эфир» (1 д.)  SJFFF0KUNLLGNH9N5AKW',
-            'СВ-98 «Люкс» (1 д.)  EZXQFAD0FD0TMPKKGYTL',
-            'HArms CQR «Эфир» (1 д.)  5I9GDP20T7FPOPD5TOCH',
-            'HArms CQR «Эфир» (1 д.)  YLEQ61UW0M3ZGZGK4N9L',
-            'АК-12 (1 д.)  URGXFKDZDONYV57KFKH8']
+            '',
+            '',
+            '',
+            '',
+            '',
+            '']
 
 
